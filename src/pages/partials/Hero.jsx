fdaac1f6c3bed5ef0612bab8ce7d6a8851e2087a
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-02-15 12:17:00 -0500

    update

--- src/pages/partials/Hero.jsx
+++ src/pages/partials/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
           {/* Hero content */}
           <div className="max-w-6xl mx-auto text-center">
             <h1 className="h2 font-hkgrotesk mb-6" data-aos="fade-up">
-              UX/UI reviews for web changes
+              UX/UI review tool for code changes
             </h1>
             <p className="text-xl text-slate-500 mb-10" data-aos="fade-up" data-aos-delay="100">
               {`React SDK that allows product stakeholders to review UX/UI changes directly in the app`}
