4e5a8babfe3fed6ca89893d46de5d9d50c35303a
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-02-20 10:50:47 -0500

    ensure try now just works on desktop

--- src/pages/partials/Cta.jsx
+++ src/pages/partials/Cta.jsx
@@ -33,10 +33,15 @@ function Cta() {
                   </span>
                 </Link>
               </div>
-              <div data-aos="fade-up" data-aos-delay="200">
+              <div className='hidden sm:block' data-aos="fade-up" data-aos-delay="200">
                 <a className="btn text-slate-300 bg-slate-700 hover:bg-slate-600 border-slate-600 w-full shadow-sm" href="https://demo.pageblox.io/?review_mode=true" target="_blank" rel="noopener noreferrer">
                   Try Now on Desktop
                 </a>
+              </div>
+              <div className='block sm:hidden' data-aos="fade-up" data-aos-delay="200">
+                <p className="btn text-slate-300 bg-slate-700 hover:bg-slate-600 border-slate-600 w-full shadow-sm">
+                  Try Now on Desktop
+                </p>
               </div>
             </div>
           </div>
