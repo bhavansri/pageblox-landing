b8a104689af116440d098b279bc82feb170a874a
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-02-20 10:41:50 -0500

    make mobile responsive

--- src/pages/partials/Features.jsx
+++ src/pages/partials/Features.jsx
@@ -46,8 +46,8 @@ function Features() {
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="flex items-center pb-12 md:pb-20">
-            <h2 className="h2 font-hkgrotesk mb-4 w-1/2">Simplify your <br/> review workflow</h2>
-            <div className="w-1/2 text-lg text-slate-300">
+            <h2 className="h3 md:h2 font-hkgrotesk mb-4 w-1/2">Simplify your <br/> review workflow</h2>
+            <div className="w-1/2 text-md md:text-lg text-slate-300">
               <p>
                 Reviewing UX/UI changes is hard. It's a time intensive & error prone process - resulting in launch delays, poor documentation, and uncaught bugs.
               </p>
@@ -107,6 +107,21 @@ function Features() {
                 </div>
               </div>
             </div>
+          </div>
+          {/* Arrows */}
+          <div className="flex md:hidden mt-12 space-x-4 justify-end">
+            <button className="carousel-prev relative z-20 w-14 h-14 rounded-full flex items-center justify-center group border border-slate-700 bg-slate-800 hover:bg-slate-700 transition duration-150 ease-in-out">
+              <span className="sr-only">Previous</span>
+              <svg className="w-4 h-4 fill-slate-400 transition duration-150 ease-in-out" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.7 14.7l1.4-1.4L3.8 9H16V7H3.8l4.3-4.3-1.4-1.4L0 8z" />
+              </svg>
+            </button>
+            <button className="carousel-next relative z-20 w-14 h-14 rounded-full flex items-center justify-center group border border-slate-700 bg-slate-800 hover:bg-slate-700 transition duration-150 ease-in-out">
+              <span className="sr-only">Next</span>
+              <svg className="w-4 h-4 fill-slate-400 transition duration-150 ease-in-out" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.3 14.7l-1.4-1.4L12.2 9H0V7h12.2L7.9 2.7l1.4-1.4L16 8z" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
