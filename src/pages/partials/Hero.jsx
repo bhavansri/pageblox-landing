ccc0a1deeafe933ff709eb1df4819403ca82f7a7
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-02-21 12:57:10 -0500

    update

--- src/pages/partials/Hero.jsx
+++ src/pages/partials/Hero.jsx
@@ -17,7 +17,7 @@ function Hero() {
               Collaborate on UX/UI reviews easily
             </h1>
             <p className="text-xl text-slate-500 mb-10" data-aos="fade-up" data-aos-delay="100">
-              A developer tool for annotating UX/UI feedback directly on code changes
+              A developer tool for reviewing UX/UI code changes directly on the app
             </p>
             <div
               className="max-w-xs mx-auto sm:max-w-none sm:inline-flex sm:justify-center space-y-4 sm:space-y-0 sm:space-x-4"
