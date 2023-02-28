b62b7e5e979aff20b96cbd0fac564a8ee222834e
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-02-27 19:23:15 -0500

    upgrade to use new version of pageblox-react

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,12 @@ import 'aos/dist/aos.css';
 import '../styles/style.css';
 import '../styles/dracula.css';
 
+import { PagebloxDndProvider } from 'pageblox-react'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PagebloxDndProvider clientId="playgroundKey">
+      <Component {...pageProps} />
+    </PagebloxDndProvider>
+  )
 }
