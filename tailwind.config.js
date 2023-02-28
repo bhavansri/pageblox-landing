6d78cd7b450ac1fdd5a3b3a0864e37b569b56566
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-02-28 02:04:49 -0500

    fix login issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/pageblox-react/**/*.js"
+    "../pageblox-react/**/*.js"
   ],
   theme: {
     extend: {
