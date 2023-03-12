4b347ae05196a727f13feab7dbded62999ceaafb
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-03-12 14:55:13 -0400

    add styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/style.css';
 import '../styles/dracula.css';
 
 import { PagebloxDndProvider } from 'pageblox-react'
+import 'pageblox-react/dist/index.css'
 
 export default function App({ Component, pageProps }) {
   return (
