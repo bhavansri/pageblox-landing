261479e0e32c01fafef410e8358150abe7b9ffa8
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-03-24 21:03:27 -0400

    update naming

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,13 @@ import 'aos/dist/aos.css';
 import '../styles/style.css';
 import '../styles/dracula.css';
 
-import { PagebloxDndProvider } from 'pageblox-react'
+import { PagebloxProvider } from 'pageblox-react'
 import 'pageblox-react/dist/index.css'
 
 export default function App({ Component, pageProps }) {
   return (
-    <PagebloxDndProvider clientId="playgroundKey">
+    <PagebloxProvider clientId="playgroundKey">
       <Component {...pageProps} />
-    </PagebloxDndProvider>
+    </PagebloxProvider>
   )
 }
