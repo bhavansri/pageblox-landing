4b84c738c4aa052fcc383783afa56239303830cb
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-04-01 15:06:49 -0400

    fix merge conflict

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,14 +9,10 @@ export default function App({ Component, pageProps }) {
   const excludePathsList = ["/login", "/register"];
 
   return (
-<<<<<<< HEAD
-    <PagebloxProvider projectId="maX1fU1LnwiquM4vAPZy">
-=======
     <PagebloxProvider
       projectId="maX1fU1LnwiquM4vAPZy"
       excludePaths={excludePathsList}
     >
->>>>>>> 251a861 (add login paths, and update npm package)
       <Component {...pageProps} />
     </PagebloxProvider>
   );
