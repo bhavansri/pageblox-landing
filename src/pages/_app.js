a2e18dfa5b86f87c8edc6ca47cb34ae010564878
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-04-04 14:33:40 -0400

    switch back

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,34 @@ import "@/styles/globals.css";
 import "aos/dist/aos.css";
 import "../styles/style.css";
 import "../styles/dracula.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   const excludePathsList = ["/login", "/register"];
 
   return (
-    <PagebloxProvider
-      projectId="maX1fU1LnwiquM4vAPZy"
-      excludePaths={excludePathsList}
-      demoMode={true}
-    >
-      <Component {...pageProps} />
-    </PagebloxProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+          `}
+      </Script>
+      <PagebloxProvider
+        projectId="maX1fU1LnwiquM4vAPZy"
+        excludePaths={excludePathsList}
+        demoMode={false}
+      >
+        <Component {...pageProps} />
+      </PagebloxProvider>
+    </>
   );
 }
