bfaf11bf2626b891f75576bccf2c8e61983e740d
Bhavan Srinathan <srinathan.bhavan@gmail.com>
2023-04-12 10:32:44 -0400

    change preview

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
-import Head from 'next/head'
-import Aos from 'aos'
-import Header from './partials/Header'
-import Hero from './partials/Hero'
-import Features from './partials/Features'
-import Features02 from './partials/Features02'
-import Faqs from './partials/Faqs'
-import Cta from './partials/Cta'
-import Integrations from './partials/Integrations'
-import { useEffect } from 'react'
+import Head from "next/head";
+import Aos from "aos";
+import Header from "./partials/Header";
+import Hero from "./partials/Hero";
+import Features from "./partials/Features";
+import Features02 from "./partials/Features02";
+import Faqs from "./partials/Faqs";
+import Cta from "./partials/Cta";
+import Integrations from "./partials/Integrations";
+import { useEffect } from "react";
 
 export default function Home() {
-
   useEffect(() => {
     Aos.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 500,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   });
 
@@ -24,11 +23,28 @@ export default function Home() {
     <>
       <Head>
         <title>Pageblox</title>
-        <meta name="title" property="og:title" content="Pageblox - UX/UI code reviews for product teams"></meta>
-        <meta name="image" property="og:image" content="/images/pageblox-og.png"></meta>
-        <meta name="description" property="og:description" content="A developer tool for product teams to manage UX/UI feedback on code changes"/>
+        <meta
+          name="title"
+          property="og:title"
+          content="Pageblox - Real-time collaborative reviews on UX/UI code changes"
+        ></meta>
+        <meta
+          name="image"
+          property="og:image"
+          content="/images/pageblox-og.png"
+        ></meta>
+        <meta
+          name="description"
+          property="og:description"
+          content="A developer tool for product teams to manage UX/UI feedback on code changes"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charset="UTF-8" /><link rel="icon" type="image/svg+xml" href="/images/pageblox-logo.svg" />
+        <meta charset="UTF-8" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/images/pageblox-logo.svg"
+        />
       </Head>
       <main className="flex flex-col min-h-screen overflow-hidden">
         <Header />
@@ -37,8 +53,8 @@ export default function Home() {
         <Integrations />
         <Features02 />
         <Faqs />
-        <Cta/>
+        <Cta />
       </main>
     </>
-  )
+  );
 }
