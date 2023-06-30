04373aa93b883923bd0422b11b085acc52f97840
Bhavan Srinathan <bhavan@gmail.com>
2023-06-30 14:36:12 -0400

    move script tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ import Faqs from "./partials/Faqs";
 import Cta from "./partials/Cta";
 import Integrations from "./partials/Integrations";
 import { useEffect } from "react";
-import Script from "next/script";
 
 export default function Home() {
   useEffect(() => {
@@ -46,10 +45,6 @@ export default function Home() {
           type="image/svg+xml"
           href="/images/pageblox-logo.svg"
         />
-        <Script>
-          {`!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t, e) { var o = e.split("."); 2 == o.length && (t = t[o[0]], e = o[1]), t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }(p = t.createElement("script")).type = "text/javascript", p.async = !0, p.src = s.api_host + "/static/array.js", (r = t.getElementsByTagName("script")[0]).parentNode.insertBefore(p, r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-          posthog.init('phc_f4fR9nqXkvFJl3vKqiDgle2aziXu1oDwk7wVoZqtc7G',{api_host:'https://app.posthog.com'})`}
-        </Script>
       </Head>
       <main className="flex flex-col min-h-screen overflow-hidden">
         <Header />
